7c653126f8bec873b3d6102cb5591fab4e604b0b
Alex Selesse <alex@selesse.com>
2014-06-08 21:42:44 -0400

    Put common functionality in one function

--- src/test/java/com/selesse/jxlint/TestFileCreator.java
+++ src/test/java/com/selesse/jxlint/TestFileCreator.java
@@ -1,25 +1,27 @@
 package com.selesse.jxlint;
 
 import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
 public class TestFileCreator {
-
-    public static File createValidXml(File baseDirectory) {
-        File file = new File(baseDirectory + File.separator + "valid.xml");
+    private static File createFile(String fileName, List<String> fileContents) {
+        File file = new File(fileName);
         try {
             boolean newFile = file.createNewFile();
             assertTrue("File creation failed, could not run test", newFile);
             file.deleteOnExit();
 
             PrintWriter fileWriter = new PrintWriter(file, Charsets.UTF_8.displayName());
-            fileWriter.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-            fileWriter.println("<empty/>");
+            for (String line : fileContents) {
+                fileWriter.println(line);
+            }
             fileWriter.flush();
             fileWriter.close();
         } catch (IOException e) {
@@ -27,81 +29,46 @@ public class TestFileCreator {
         }
 
         return file;
+    }
+
+    public static File createValidXml(File baseDirectory) {
+        List<String> fileContents = Lists.newArrayList(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+                "<empty/>"
+        );
+
+        return createFile(baseDirectory + File.separator + "valid.xml", fileContents);
     }
 
     public static File createBadEncodingFile(File baseDirectory) {
-        File file = new File(baseDirectory + File.separator + "bad-encoding.xml");
-        try {
-            boolean newFile = file.createNewFile();
-            assertTrue("File creation failed, could not run test", newFile);
-            file.deleteOnExit();
-
-            PrintWriter fileWriter = new PrintWriter(file, Charsets.UTF_8.displayName());
-            fileWriter.println("<?xml version=\"1.0\"?>");
-            fileWriter.println("<empty/>");
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return file;
+        List<String> fileContents = Lists.newArrayList(
+                "<?xml version=\"1.0\"?>",
+                "<empty/>"
+        );
+        return createFile(baseDirectory + File.separator + "bad-encoding.xml", fileContents);
     }
 
     public static File createBadAttributeFile(File baseDirectory) {
-        File file = new File(baseDirectory + File.separator + "bad-attribute.xml");
-        try {
-            boolean newFile = file.createNewFile();
-            assertTrue("File creation failed, could not run test", newFile);
-            file.deleteOnExit();
-
-            PrintWriter fileWriter = new PrintWriter(file, Charsets.UTF_8.displayName());
-            fileWriter.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-            fileWriter.println("<attribute name=\"dupe-name\" name=\"dupe-name\"/>");
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return file;
+        List<String> fileContents = Lists.newArrayList(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+                "<attribute name=\"dupe-name\" name=\"dupe-name\"/>"
+        );
+        return createFile(baseDirectory + File.separator + "bad-attribute.xml", fileContents);
     }
 
     public static File createBadVersionFile(File baseDirectory) {
-        File file = new File(baseDirectory + File.separator + "bad-version.xml");
-        try {
-            boolean newFile = file.createNewFile();
-            assertTrue("File creation failed, could not run test", newFile);
-            file.deleteOnExit();
-
-            PrintWriter fileWriter = new PrintWriter(file, Charsets.UTF_8.displayName());
-            fileWriter.println("<?xml encoding=\"UTF-8\"?>");
-            fileWriter.println("<empty/>");
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return file;
+        List<String> fileContents = Lists.newArrayList(
+                "<?xml encoding=\"UTF-8\"?>",
+                "<empty/>"
+        );
+        return createFile(baseDirectory + File.separator + "bad-version.xml", fileContents);
     }
 
     public static File createBadAuthorFile(File baseDirectory) {
-        File file = new File(baseDirectory + File.separator + "author.xml");
-        try {
-            boolean newFile = file.createNewFile();
-            assertTrue("File creation failed, could not run test", newFile);
-            file.deleteOnExit();
-
-            PrintWriter fileWriter = new PrintWriter(file, Charsets.UTF_8.displayName());
-            fileWriter.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-            fileWriter.println("<author name=\"\"/>");
-            fileWriter.flush();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return file;
+        List<String> fileContents = Lists.newArrayList(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+                "<author name=\"\"/>"
+        );
+        return createFile(baseDirectory + File.separator + "author.xml", fileContents);
     }
 }
